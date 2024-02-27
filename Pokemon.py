d17f8b1b7ba9b7d33a0f94ffae3d60a81f0296a8
Davron <152484311+Davron030901@users.noreply.github.com>
2024-02-27 23:44:13 +0500

    Add files via upload

---
+++ Pokemon.py
@@ -0,0 +1,14 @@
+# print("|  Pokemon Name  |  Type  |")
+# print("___________________________")
+from prettytable import PrettyTable
+
+table=PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+
+table.align="l"
+
+
+
+
+print(table)
